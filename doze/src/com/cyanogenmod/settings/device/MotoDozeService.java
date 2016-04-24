CyanogenMod/android_device_motorola_msm8610-common

msm8610-common: MotoDoze: Hold a wakelock when the screen is on
Sometimes the device is suspended before ACTION_SCREEN_OFF is received.
When this happens, the proximity sensor doesn't get enabled, breaking
the wake gesture. Hold a wakelock when the screen is on and release it
only once the sensor has been enabled.

Change-Id: I320d4bbc4aea2f2f7e3ba9bdf854af08a97631fd

doze/src/com/cyanogenmod/settings/device/MotoDozeService.java
@@ -47,6 +47,7 @@ public class MotoDozeService extends Service {
     private Context mContext;
     private MotoProximitySensor mSensor;
     private PowerManager mPowerManager;
+    private PowerManager.WakeLock mWakeLock;
 
     private boolean mHandwaveGestureEnabled = false;
     private boolean mPocketGestureEnabled = false;
@@ -111,6 +112,7 @@ public class MotoDozeService extends Service {
         mContext = this;
         mPowerManager = (PowerManager)getSystemService(Context.POWER_SERVICE);
         mSensor = new MotoProximitySensor(mContext);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MotoDozeWakeLock");
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
         loadPreferences(sharedPrefs);
         sharedPrefs.registerOnSharedPreferenceChangeListener(mPrefListener);
@@ -149,12 +151,20 @@ public class MotoDozeService extends Service {
     private void onDisplayOn() {
         if (DEBUG) Log.d(TAG, "Display on");
         mSensor.disable();
+        if (areGesturesEnabled() && !mWakeLock.isHeld()) {
+            if (DEBUG) Log.d(TAG, "Acquiring wakelock");
+            mWakeLock.acquire();
+        }
     }
 
     private void onDisplayOff() {
         if (DEBUG) Log.d(TAG, "Display off");
         if (isDozeEnabled()) {
             mSensor.enable();
+        }
+        if (mWakeLock.isHeld()) {
+            if (DEBUG) Log.d(TAG, "Releasing wakelock");
+            mWakeLock.release();
         }
     }
 
@@ -182,6 +192,11 @@ public class MotoDozeService extends Service {
                 mHandwaveGestureEnabled = sharedPreferences.getBoolean(GESTURE_HAND_WAVE_KEY, false);
             } else if (GESTURE_POCKET_KEY.equals(key)) {
                 mPocketGestureEnabled = sharedPreferences.getBoolean(GESTURE_POCKET_KEY, false);
+            }
+
+            if (areGesturesEnabled() && !mWakeLock.isHeld()) {
+                if (DEBUG) Log.d(TAG, "Acquiring wakelock");
+                mWakeLock.acquire();
             }
         }
     };

AOKP/device_motorola_msm8610-common

doze/src/com/cyanogenmod/settings/device/MotoDozeService.java
@@ -47,6 +47,7 @@ public class MotoDozeService extends Service {
     private Context mContext;
     private MotoProximitySensor mSensor;
     private PowerManager mPowerManager;
+    private PowerManager.WakeLock mWakeLock;
 
     private boolean mHandwaveGestureEnabled = false;
     private boolean mPocketGestureEnabled = false;
@@ -111,6 +112,7 @@ public class MotoDozeService extends Service {
         mContext = this;
         mPowerManager = (PowerManager)getSystemService(Context.POWER_SERVICE);
         mSensor = new MotoProximitySensor(mContext);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MotoDozeWakeLock");
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
         loadPreferences(sharedPrefs);
         sharedPrefs.registerOnSharedPreferenceChangeListener(mPrefListener);
@@ -149,12 +151,20 @@ public class MotoDozeService extends Service {
     private void onDisplayOn() {
         if (DEBUG) Log.d(TAG, "Display on");
         mSensor.disable();
+        if (isDozeEnabled() && !mWakeLock.isHeld()) {
+            if (DEBUG) Log.d(TAG, "Acquiring wakelock");
+            mWakeLock.acquire();
+        }
     }
 
     private void onDisplayOff() {
         if (DEBUG) Log.d(TAG, "Display off");
         if (isDozeEnabled()) {
             mSensor.enable();
+        }
+        if (mWakeLock.isHeld()) {
+            if (DEBUG) Log.d(TAG, "Releasing wakelock");
+            mWakeLock.release();
         }
     }
 
@@ -182,6 +192,11 @@ public class MotoDozeService extends Service {
                 mHandwaveGestureEnabled = sharedPreferences.getBoolean(GESTURE_HAND_WAVE_KEY, false);
             } else if (GESTURE_POCKET_KEY.equals(key)) {
                 mPocketGestureEnabled = sharedPreferences.getBoolean(GESTURE_POCKET_KEY, false);
+            }
+
+            if (isDozeEnabled() && !mWakeLock.isHeld()) {
+                if (DEBUG) Log.d(TAG, "Acquiring wakelock");
+                mWakeLock.acquire();
             }
         }
     };